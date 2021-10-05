37f3ea3ae3cc5f5e60164424d036cfaa465bb35c
Alex <Alex@LAPTOP-GV3B110K>
2021-10-05 15:57:40 +0200

    Добавьте файлы проекта.

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число А");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число B");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число C");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int dla=a;
+            int dlb = b;
+            int i1 = 0;
+
+            while (dlb >= c)
+
+            {
+                dla = a;
+                while (dla >= c)
+                {
+                    dla -= c;
+                    i1++;
+                }
+                dlb -= c;
+            }
+            Console.WriteLine("Число квадратов {0}", i1);
+            Console.ReadKey();
+        }
+    }
+}
